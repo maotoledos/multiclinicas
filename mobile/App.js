92b28050b885b64c6b2e116e26a4549faec0e09d
maotoledos <mauricioj.toledo@gmail.com>
2019-10-13 14:53:22 -0600

    lumas changes

--- mobile/App.js
+++ mobile/App.js
@@ -1,17 +1,42 @@
 import React, {Component} from 'react';
-import {Image, StyleSheet, Text, View,FlatList,StatusBar, Button, Alert} from 'react-native';
+import {Image, StyleSheet, Text, View,FlatList,StatusBar, Button} from 'react-native';
+import Constants from 'expo-constants';
 import axios from 'axios';
 import { queryBuilder } from './setup/helpers';
-
 export default class App extends Component<Props> {
+  
 
   state ={
-    data:[]
+    data:[],
+    visibleLogin: false
   }
+  showLoginModal = () => {
+    this.setState({
+        visibleLogin: true,
+    });
+    
+};
 
+  constructor(props) {
+    super(props)
+    
+    this.state = {
+      lastRefresh: Date(Date.now()).toString(),
+    }
+    
+    this.refreshScreen = this.refreshScreen.bind(this)
+  }
+  
+  refreshScreen() {
+    //this.setState({ lastRefresh: Date(Date.now()).toString() })
+    this.fetchData()
+  }
+  
+  
   fetchData= async()=>{
+  
 
-    axios.post("http://192.168.1.46:3000", queryBuilder({
+    axios.post("http://192.168.1.10:3000", queryBuilder({
 
         type: 'query',
         operation: 'inventarios',
@@ -20,6 +45,7 @@ export default class App extends Component<Props> {
       }))
       .then((response) => {
         let respuesta = response.data.data.inventarios;
+        console.log(respuesta)
         
         this.setState({data: respuesta});
         
@@ -28,21 +54,25 @@ export default class App extends Component<Props> {
         console.log(error)
       })
   }
+  
 componentDidMount(){
   this.fetchData();
 }
+
   render() {
+  
     return (
-      <View >
-        <Text></Text>
+         <View style={{flex: 1, backgroundColor: 'black'}}>
+            <View style={[styles.box, styles.box1]}></View>
+            <View style={[styles.box, styles.box2]}></View>
+            <View style={[styles.box, styles.box3]}></View>
         <StatusBar backgroundColor="blue" barStyle="light-content" />
-       <Text>CLINICAS MEDICAS</Text>
-      
+        <Button onPress={this.refreshScreen} title="" />
+        <Button onPress={this.refreshScreen} title="Refresh Screen" />
        <FlatList
        data={this.state.data}
        keyExtractor={(item,index) => index.toString()}
        renderItem={({item}) =>
-
        <View style={{backgroundColor:'#abc123',padding:10,margin:10}}>
           <Text style={{color:'#fff', fontWeight:'bold'}}>
           Medicina: {item['medicinaId'].id}
@@ -55,17 +85,16 @@ componentDidMount(){
           <Text style={{color:'#fff'}}>Lote: {item.lote}</Text>
           <Image
           style={{width: 50, height: 50}}
-          source={{uri: 'https://facebook.github.io/react-native/img/tiny_logo.png'}}
+          source={{uri: 'https://images2.revistafama.com/SGkcBjuXI-4eJVyxC5FeCRpyUrs=/958x596/uploads/media/2019/05/23/pastillas-anticonceptivas-de-emergencia.jfif'}}
         />
+       
          </View>
+         
 
        }
 
        />
-       <Button
-          title="Press me"
-          onPress={() => Alert.alert('Simple Button pressed')}
-        />
+ 
       </View>
     );
   }
